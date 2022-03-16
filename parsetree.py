918a22be71b4a88cb1300efc930e1664962aec5b
Savita Pal <savita.pal08@gmail.com>
2022-03-16 09:05:12 +0000

    Committed changes

---
+++ parsetree.py
@@ -0,0 +1,13 @@
+import xml.etree.ElementTree as ET
+
+def createflow(filename, proxytettag, tagtype):
+        tree = ET.parse(filename)
+        root = tree.getroot()
+        for child in root:
+            if child.tag == proxytettag:
+                for subchild in child:
+                    if subchild.tag == tagtype:
+                        for sub_subchild in subchild:
+                            print(sub_subchild[0].text)
+
+createflow('default.xml', "PreFlow", "Request")
